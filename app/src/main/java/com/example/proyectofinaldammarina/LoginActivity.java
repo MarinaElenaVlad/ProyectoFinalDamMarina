59c123e15e98e1cdbb734ee942644ada071cdc81
Marina <marinaelenavlad@gmail.com>
2023-06-01 14:38:45 +0200

    Se ha hecho el front de la parte de logueo y registro.

---
+++ app/src/main/java/com/example/proyectofinaldammarina/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.proyectofinaldammarina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * AÑADIR EXPLICACION + COMENTARIOS
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    Button boton_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
