db1f7b23bbd6d74f5cd409630a64fcd557b778cb
Marina <marinaelenavlad@gmail.com>
2023-06-01 14:53:48 +0200

    Se ha hecho el front de la parte de logueo y registro.

--- app/src/main/java/com/example/proyectofinaldammarina/LoginActivity.java
+++ app/src/main/java/com/example/proyectofinaldammarina/LoginActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 /**
- * AÑADIR EXPLICACION + COMENTARIOS
+ * AÑADIR EXPLICACION + COMENTARIOSs
  */
 public class LoginActivity extends AppCompatActivity {
 
